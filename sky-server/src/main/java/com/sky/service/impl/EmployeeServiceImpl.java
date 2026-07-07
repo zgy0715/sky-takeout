28c5300a7350a99111630e8c16d5cdbbf1f07839
YZBzgy <zgy0715@qq.com>
2026-07-07 14:38:31 +0800

    初始化

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
+import java.nio.charset.StandardCharsets;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
@@ -37,7 +39,8 @@ public class EmployeeServiceImpl implements EmployeeService {
             //账号不存在
             throw new AccountNotFoundException(MessageConstant.ACCOUNT_NOT_FOUND);
         }
-
+        //加密
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         //密码比对
         // TODO 后期需要进行md5加密，然后再进行比对
         if (!password.equals(employee.getPassword())) {
