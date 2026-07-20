bd86d964bbc47d8aef78236a2603844fd47f1eb3
YZBzgy <zgy0715@qq.com>
2026-07-20 19:28:23 +0800

    更新

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,10 +1,12 @@
 package com.sky.service;
 
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import org.springframework.beans.BeanUtils;
+import org.springframework.util.DigestUtils;
 
 public interface EmployeeService {
 
@@ -27,13 +29,14 @@ public interface EmployeeService {
 
         //设置账号的状态，默认正常状态1表示正常，0表示锁定
         employee.setStatus(StatusConstant.ENABLE);
+        //设置密码，默认密码是123456
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
 
 
 
 
 
 
-        
 
     }
 
